a31e0c914d3b25e3555f3d145e9153a11b251a02
Tanya Hodges <tanyac52003@gmail.com>
2020-07-20 19:22:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,8 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end 
   
-  
+  get '/:operation/:number1/:number2' do
+    
+  end 
 
 end
