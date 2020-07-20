62322bf9c76b6c081e0080063bbbf4dafe3a5a29
Tanya Hodges <tanyac52003@gmail.com>
2020-07-20 19:25:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,10 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     string = ""
-    
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
