378faacb0fbad8532acea1d78b402ec23b2cbe54
Tanya Hodges <tanyac52003@gmail.com>
2020-07-20 19:04:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do
+    @reverse_name = params[:name].reverse 
+    @reverse_name
+  end 
+  
+  get '/squarenumber/:num' do 
+    @num = params[:num].to_i **2 
+    @num.to_s 
+  end 
 
 end
